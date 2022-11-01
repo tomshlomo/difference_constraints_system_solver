1b7c1f144ad9ceee2ef3dc03d4e8070c19773351
tom.shlomo <tom.shlomo@ridewithvia.com>
2022-11-01 17:30:39 +0200

    no clippy warnings

--- src/paper_version.rs
+++ src/paper_version.rs
@@ -1,159 +1,189 @@
 use priority_queue::PriorityQueue;
 use std::collections::HashMap;
-use std::fmt::Debug;
+use std::fmt::{Debug, Display};
 use std::hash::Hash;
 
-pub trait VarId: Eq + Hash + Debug + Clone {}
-impl<T> VarId for T where T: Eq + Hash + Debug + Clone {}
+pub trait VarId: Eq + Hash + Debug + Clone + Display {}
+impl<T> VarId for T where T: Eq + Hash + Debug + Clone + Display {}
 
 #[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
-enum Node<T: VarId> {
-    Source,
-    Variable(T),
-}
-
-#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
-pub struct Constraint<T: VarId>{
+pub struct Constraint<T: VarId> {
     // v - u <= c
     pub v: T,
     pub u: T,
     pub c: i64,
 }
-impl <T: VarId> Constraint<T> {
+impl<T: VarId> Constraint<T> {
     pub fn new(v: T, u: T, c: i64) -> Self {
-        Constraint{v: u, u: v, c: c}
+        Constraint { v, u, c }
+    }
+}
+
+impl<T: VarId> Display for Constraint<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} - {} <= {}", self.v, self.u, self.c)
     }
 }
 
 #[derive(Debug, Clone)]
-pub struct Solution<T: VarId>(HashMap<Node<T>, i64>);
+pub struct Solution<T: VarId>(HashMap<T, i64>);
 
 impl<T: VarId> Solution<T> {
     pub fn new() -> Solution<T> {
-        let mut map = HashMap::new();
-        map.insert(Node::Source, 0);
+        let map = HashMap::new();
         Solution(map)
     }
-    fn update(&mut self, node: &Node<T>, val: i64) {
-        self.0.insert(node.clone(), val);
+    fn update(&mut self, var: &T, val: i64) {
+        self.0.insert(var.clone(), val);
     }
     pub fn add_var_if_missing(&mut self, var: T) {
-        let node = Node::Variable(var);
-        if !self.0.contains_key(&node) {
-            self.update(&node, 0);
+        // todo: use entry API
+        if !self.0.contains_key(&var) {
+            self.update(&var, 0);
         };
     }
-    pub fn get(&self, var: &T) -> i64 {
-        self.safe_get_node(&Node::Variable(var.clone())).unwrap_or(&0).clone()
+    pub fn get_or(&self, var: &T, default: i64) -> i64 {
+        *self.get(var).unwrap_or(&default)
     }
-    fn get_node(&self, node: &Node<T>) -> i64 {
-        self.safe_get_node(node).unwrap().clone()
-    }
-     fn safe_get_node(&self, node: &Node<T>) -> Option<&i64> {
-        self.0.get(node)
+    fn get(&self, var: &T) -> Option<&i64> {
+        self.0.get(var)
     }
     pub fn check_constraint(&self, constraint: &Constraint<T>) -> bool {
         // v - u <= c
-        if let (
-            Some(u_val),
-            Some(v_val),
-            ) = (
-            self.safe_get_node(&Node::Variable(constraint.u.clone())), 
-            self.safe_get_node(&Node::Variable(constraint.v.clone())),
-        ) {
-            return v_val - u_val <= constraint.c
+        if let (Some(u_val), Some(v_val)) = (self.get(&constraint.u), self.get(&constraint.v)) {
+            return v_val - u_val <= constraint.c;
         }
         true
+    }
+}
+
+impl<T: VarId> Default for Solution<T> {
+    fn default() -> Self {
+        Self::new()
     }
 }
 
 pub struct DCS<T: VarId> {
-    // graph: DiGraphMap<Node, i64>,
-    succesors: HashMap<Node<T>, Vec<(T, i64)>>,
+    succesors: HashMap<T, Vec<(T, i64)>>,
 }
 
 impl<T: VarId> DCS<T> {
-    fn new() -> Self {
-        let mut succesors = HashMap::new();
-        succesors.insert(Node::Source, vec![]);
-        DCS {
-            succesors: succesors,
-        }
+    //todo: implement implied_lb and implied_ub
+    pub fn new() -> Self {
+        let succesors = HashMap::new();
+        DCS { succesors }
     }
     pub fn is_variable(&self, x: &T) -> bool {
-        self.succesors.contains_key(&Node::Variable(x.clone()))
+        self.succesors.contains_key(x)
     }
     pub fn add_unconstrained_variable(&mut self, x: &T) {
         if !self.is_variable(x) {
-            let node = Node::Variable(x.clone());
-            self.add_succesor(&Node::Source, x, 0);
-            self.succesors.insert(node, vec![]);
+            self.succesors.insert(x.clone(), vec![]);
         }
     }
     fn var_constraints(&self, u: &T) -> Vec<Constraint<T>> {
-        self.succesors.get(&Node::Variable(u.clone())).unwrap_or(&vec![]).iter().map(
-            |(v, c)| {
-                Constraint::new(v.clone(), u.clone(), c.clone())
-            }
-        ).collect()
+        // todo: just return an iterator, not a vec
+        self.succesors
+            .get(u)
+            .unwrap_or(&vec![])
+            .iter()
+            .map(|(v, c)| Constraint::new(v.clone(), u.clone(), *c))
+            .collect()
     }
     fn constraints(&self) -> Vec<Constraint<T>> {
-        // not great
-        self.succesors.keys().into_iter().map(|node| {
-            match node {
-                Node::Source => vec![],
-                Node::Variable(var) => self.var_constraints(var)
-            }
-        }).flat_map(|s| s.clone()).collect()
+        // todo: not great
+        self.succesors
+            .keys()
+            .into_iter()
+            .flat_map(|var| self.var_constraints(var))
+            .collect()
     }
     pub fn check_solution(&self, sol: &Solution<T>) -> bool {
         for constraint in self.constraints() {
             if !sol.check_constraint(&constraint) {
-                return false
+                println!(
+                    "constraint {} is not satisfied by the solution {:#?}",
+                    constraint, sol.0
+                );
+                return false;
             }
         }
         true
     }
-    fn add_succesor(&mut self, from_node: &Node<T>, to_variable: &T, c: i64) {
+    fn add_succesor(&mut self, from_var: &T, to_variable: &T, c: i64) {
         self.succesors
-            .get_mut(from_node)
+            .get_mut(from_var)
             .expect("variable should be added")
             .push((to_variable.clone(), c));
     }
-    pub fn add_to_feasible(&mut self, constraint: &Constraint<T>, sol: &Solution<T>) -> Option<Solution<T>> {
+    pub fn add_to_feasible(
+        &mut self,
+        constraint: &Constraint<T>,
+        sol: &Solution<T>,
+    ) -> Option<Solution<T>> {
+        println!("adding constraint {}", constraint);
+        println!("current solution is: {:#?}", sol.0);
         self.add_unconstrained_variable(&constraint.v);
         self.add_unconstrained_variable(&constraint.u);
-        let v_node = Node::Variable(constraint.v.clone());
-        let u_node = Node::Variable(constraint.u.clone());
-        self.add_succesor(&u_node, &constraint.v, constraint.c);
-        let mut q = PriorityQueue::new();
+        self.add_succesor(&constraint.u, &constraint.v, constraint.c);
         let mut new_sol = sol.clone();
-        q.push(v_node, 0);
+        new_sol.add_var_if_missing(constraint.u.clone());
+        new_sol.add_var_if_missing(constraint.v.clone());
+        if new_sol.check_constraint(constraint) {
+            return Some(new_sol)
+        }
+
+        let mut q = PriorityQueue::new();
+        q.push(&constraint.v, 0);
         loop {
             if let Some((x, dist_x)) = q.pop() {
-                let new_val = sol.safe_get_node(&u_node).unwrap_or(&0) + constraint.c + dist_x;
-                let d_x = sol.safe_get_node(&x).unwrap_or(&0).clone();
+                let new_val = sol.get(&constraint.u).unwrap_or(&0) + constraint.c + dist_x;
+                let d_x = *sol.get(x).unwrap_or(&0);
                 if new_val < d_x {
-                    if x == u_node {
-                        self.succesors.get_mut(&u_node).unwrap().pop(); // remove the new constraint
+                    if x == &constraint.u {
+                        self.succesors.get_mut(&constraint.u).unwrap().pop(); // remove the new constraint
+                        print!("System is infeasible");
                         return None;
-                    } 
-                    new_sol.update(&x, new_val);
-                    for (y, x2y) in self.succesors.get(&x).unwrap() {
-                        let scaled_path_len = dist_x + d_x + x2y - sol.get(y);
-                        q.push_decrease(Node::Variable(y.clone()), scaled_path_len);
+                    }
+                    new_sol.update(x, new_val);
+                    for (y, x2y) in self.succesors.get(x).unwrap() {
+                        let scaled_path_len = dist_x + d_x + x2y - sol.get_or(y, 0);
+                        q.push_decrease(y, scaled_path_len);
                     }
                 }
             } else {
+                println!("new solution is: {:#?}", new_sol.0);
                 return Some(new_sol);
             }
         }
     }
 }
-#[cfg(test)]
 
+impl<T: VarId> Default for DCS<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+#[cfg(test)]
 mod tests {
     use super::*;
+    fn check_multiple_constraints<T: VarId, I: Iterator<Item = (T, T, i64)>>(
+        constraints: I,
+    ) -> Solution<T> {
+        let mut sys = DCS::new();
+        let mut sol = Solution::new();
+        for (v, u, c) in constraints {
+            let constraint = Constraint::new(v, u, c);
+            if let Some(new_sol) = sys.add_to_feasible(&constraint, &sol) {
+                assert!(sys.check_solution(&new_sol));
+                sol = new_sol;
+            } else {
+                panic!()
+            }
+        }
+        sol
+    }
 
     #[test]
     fn test_check() {
@@ -163,5 +193,28 @@ mod tests {
         let sol = Solution::new();
         let new_sol = sys.add_to_feasible(&Constraint::new(x, y, 0), &sol);
         assert!(sys.check_solution(&new_sol.unwrap()))
+    }
+
+    #[test]
+    fn test_check2() {
+        check_multiple_constraints(
+            [
+                ("y".to_owned(), "x".to_owned(), 1),
+                ("z".to_owned(), "y".to_owned(), 2),
+                ("x".to_owned(), "z".to_owned(), -3),
+                ("z".to_owned(), "x".to_owned(), 4),
+            ]
+            .into_iter(),
+        );
+        println!("hello")
+    }
+
+    #[test]
+    fn test_check3() {
+        let x = "x";
+        let y = "y";
+        let z = "z";
+        check_multiple_constraints([(y, x, 1), (z, y, 2), (x, z, -3), (z, x, 4)].into_iter());
+        println!("hello")
     }
 }
